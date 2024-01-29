7aaeb330bed75f766cc047abe76c5f7d4c89ed30
MDWojcieszak <wojcieszak.4@gmail.com>
2024-01-29 18:26:34 +0100

    feature(theme): app palette

--- src/utils/theme/colors.ts
+++ src/utils/theme/colors.ts
@@ -3,7 +3,21 @@ const palette = {
   black: '#000000',
 } as const;
 
+const fallSeasonPalette = {
+  gray01: '333944',
+  gray02: '#292d38',
+  gray03: '#1f222a',
+  gray04: '#14171c',
+  gray05: '#0B0C0D',
+  mainGreen: '#359E7A',
+  lightGreen: '#8CCF77',
+  yellow: '#F9F871',
+  blue: '#009DF8',
+  lightBlue: '#D1F5FF',
+};
+
 export const colors = {
-  background: palette.black,
+  background: fallSeasonPalette.gray05,
+  ...fallSeasonPalette,
   ...palette,
 } as const;
