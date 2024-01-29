d6e10a18931e0e793eef91911fddb22113b950bc
MDWojcieszak <wojcieszak.4@gmail.com>
2024-01-29 18:28:03 +0100

    feature(hooks): useResponsive

---
+++ src/hooks/useResponsive.tsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { ResponsiveContext } from '~/contexts/Responsive/ResponsiveContext';
+
+export const useResponsive = () => {
+  const ctx = useContext(ResponsiveContext);
+
+  if (!ctx) throw new Error('Use this hook only in ResponsiveContextProvider!');
+
+  return ctx;
+};
