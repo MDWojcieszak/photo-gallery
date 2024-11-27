dd145019e77d01ab7478ac5d5689732ca25046de
MDWojcieszak <wojcieszak.4@gmail.com>
2024-11-27 20:58:33 +0100

    change(components): about me

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,9 +6,9 @@ export const AboutMe = () => {
   const paragraphDelays = [0, 6, 12];
 
   const text = [
-    'Hi! I’m a passionate photographer 📸 who loves capturing the beauty of nature 🌳, stunning sunsets 🌅, majestic mountains 🏔️, and the unique atmosphere of interesting architectural spots 🏙️. I have a special appreciation for cityscapes at night 🌃—there’s something captivating about the way lights transform urban landscapes. 🌆',
-    'I’m a big fan of Fujifilm, drawn to its amazing color science 🎨 and vintage-inspired design 🖼️. While I usually rely on my digital gear 💻, I occasionally dive into analog photography 📷, enjoying the charm and unpredictability of shooting on film 🎞️.',
-    'Traveling and exploring new places fuels my creativity ✈️. Photography isn’t just something I do—it’s how I connect with the world around me 🌍, one frame at a time.',
+    'Hi! I’m a passionate photographer who loves capturing the beauty of nature, stunning sunsets, majestic mountains and the unique atmosphere of interesting architectural spots around the world. I have a special appreciation for cityscapes at night — there’s something captivating about the way lights transform urban landscapes.',
+    'I’m a big fan of Fujifilm, drawn to its amazing color science and vintage-inspired design. While I usually rely on my digital gear, I occasionally dive into analog photography.',
+    'Photography isn’t just something I do — it’s how I connect with the world around me 📸 one fram at a time.',
   ];
 
   return (
