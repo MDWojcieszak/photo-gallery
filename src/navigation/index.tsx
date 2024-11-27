74db0e9d6e57dcb7555810a07ffcc22205ccfa54
MDWojcieszak <wojcieszak.4@gmail.com>
2024-11-27 19:11:22 +0100

    feature(navigation): about, and no route

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,19 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { About } from '~/routes/About';
 import { Dashboard } from '~/routes/Dashboard';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Dashboard />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/*',
+    element: <Navigate to='/' />,
   },
 ]);
 
